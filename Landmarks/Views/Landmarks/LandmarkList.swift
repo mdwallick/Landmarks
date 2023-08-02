0f385090e17cff37ac607322cd4b865353d05b8a
Mike Wallick <mike@wallick.net>
2023-08-02 16:44:10 -0500

    Initial commit

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,49 @@
+//
+//  LandmarksList.swift
+//  Landmarks
+//
+//  Created by Mike Wallick on 8/2/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Landmarks")
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+//        ForEach(["iPhone SE (3nd generation)", "iPhone 14"], id: \.self) { deviceName in
+//                    LandmarkList()
+//                        .previewDevice(PreviewDevice(rawValue: deviceName))
+//                        .previewDisplayName(deviceName)
+//                }
+    }
+}
